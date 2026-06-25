fb984afbaa3bed0a9c1feb6a3ea1d09dc17c21b5
L A Minora <leonardo.minora@gmail.com>
2026-06-25 12:06:54 -0300

    fix: merge do indexl/css com ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ function createWindow(): void {
 	const win = new BrowserWindow({
 		width: 1200,
 		height: 800,
+		autoHideMenuBar: true,
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js"),
 			contextIsolation: true,
 			nodeIntegration: false,
+			sandbox: true,
 		},
 	});
 
